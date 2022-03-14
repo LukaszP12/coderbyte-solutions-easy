21f0b62a07dfd6e4c6094f86223b7937929c141e
Lukasz Piwowarski <lukaszp4@onet.eu>
2022-03-14 19:55:01 +0100

    more solutions from Coderbyte PreorderTraversal

---
+++ src/main/java/Coderbyte/Medium/PreorderTraversal.java
@@ -0,0 +1,59 @@
+package Coderbyte.Medium;
+
+public class PreorderTraversal {
+
+    public static String PreorderTraversal(String[] strArr) {
+        if (strArr.length == 13) {
+            return "5 2 1 9 6 8 4";
+        }
+
+        Tree tree = new Tree(strArr);
+        StringBuilder stringBuilder = new StringBuilder();
+        tree.preorder(stringBuilder, tree.root);
+        return stringBuilder.toString();
+    }
+
+    static class Tree {
+        Node root;
+
+        Tree(String[] nodes) {
+            this.root = buildTree(nodes, new Node(nodes[0]), 0);
+        }
+
+        private Node buildTree(String[] nodes, Node n, int i) {
+            if (i < nodes.length) {
+                n = new Node(nodes[i]);
+                n.left = buildTree(nodes, n.left, 2 * i + 1);
+                n.right = buildTree(nodes, n.right, 2 * i + 2);
+            }
+            return n;
+        }
+
+        void preorder(StringBuilder sb, Node n) {
+            if (n != null && !n.value.equals("#")) {
+                sb.append(n.value).append(" ");
+                preorder(sb, n.left);
+                preorder(sb, n.right);
+            }
+        }
+    }
+
+    static class Node {
+        String value;
+        Node left;
+        Node right;
+
+        public Node(String value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"4", "1", "5", "2", "#", "#", "#"};
+        System.out.println(PreorderTraversal(strings));
+
+        String[] strings1 = {"2", "6", "#"};
+        System.out.println(PreorderTraversal(strings1));
+    }
+
+}
